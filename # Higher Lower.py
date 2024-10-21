9862126e451c7c4e6ad4aed6134721898f7b19f7
bellayun <yunbella1@gmail.com>
2024-10-21 12:55:11 -0400

    jk

---
+++ # Higher Lower.py
@@ -0,0 +1,61 @@
+# Higher Lower
+# Higher followers or lower followers?
+
+data = [
+    {
+        'name' : 'Instagram',
+        'follower_count' : 900,
+        'description' : 'Social media platform',
+        'country' : 'United States'
+    },
+    {
+        'name' : 'Christiano Ronaldo',
+        'follower_count' : 500,
+        'description' : 'Footballer',
+        'country' : 'Portugal'
+    },
+    {
+        'name' : 'Ariana Grande',
+        'follower_count' : 189,
+        'description' : 'Musician and actress',
+        'country' : 'United States'
+    },
+    {
+        'name' : 'Swayne Johnson',
+        'follower_count' : 165,
+        'description' : 'Actor and professional wrestler',
+        'country' : 'United States'
+    },
+    {
+        'name' : 'Lando Norris',
+        'follower_count' : 297,
+        'description' : 'F1 Driver - McLaren',
+        'country' : 'England'
+    },
+    {
+        'name' : 'Max Verstappen',
+        'follower_count' : 354,
+        'description' : 'F1 Driver - Red Bull Racing',
+        'country' : 'France'
+    },
+    {
+        'name' : 'George Russell',
+        'follower_count' : 167,
+        'description' : 'F1 Driver - Mercedes',
+        'country' : 'England'
+    },
+    {
+        'name' : 'Nico Hulkenberg',
+        'follower_count' : 29,
+        'description' : 'F1 Driver - Haas',
+        'country' : 'Germany'
+    },
+    {
+        'name' : 'Oliver Bearman',
+        'follower_count' : 7,
+        'description' : 'F1 Driver - Haas',
+        'country' : 'England'
+    },
+]
+
+# Generate a random aacount from the agme data.
