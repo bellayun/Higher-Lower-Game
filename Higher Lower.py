c2d073ebdde6e27d4a4a4bf59832f717f14842a3
bellayun <yunbella1@gmail.com>
2024-10-21 13:19:09 -0400

    final

--- Higher Lower.py
+++ Higher Lower.py
@@ -12,24 +12,47 @@ def format_data(account):
     return f"{account_name}, a {account_descr}, from {account_country}"
 
 
-# Generate a random aacount from the agme data.
-account_a = random.choice(data)
+def check_answer(user_guess, a_followers, b_followers):
+    """Take a user's guess and the follower counts and returns if this is correct"""
+    if a_followers > b_followers:
+        return user_guess == "a"
+    else:
+        return user_guess == "b"
+    
+
+score = 0
+game_should_continue = True
 account_b = random.choice(data)
-if account_a == account_b :
+
+# Generate a random aacount from the agme data.
+while game_should_continue:
+    account_a = account_b
+    # account_a = random.choice(data)
     account_b = random.choice(data)
+    if account_a == account_b :
+        account_b = random.choice(data)
 
-print(f"Compare A: {format_data(account_a)}.")
-print(f"Compare B: {format_data(account_b)}.")
+    print(f"Compare A: {format_data(account_a)}.")
+    print(f"Against B: {format_data(account_b)}.")
 
-# Ask user for a guess
+    # Ask user for a guess
+    guess = input("Who has higer followers A or B?: ").lower()
+
+    # Check if user is correct
+    ## Get follower count of each account
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
 
-# Check if user is correct
-## Get follower count of each account
-## Use if statement to check if user is correct
-
-
-# Give user feedback on their guess
+    # Give user feedback on their guess
+    if is_correct:
+        score += 1
+        print(f"You are right. Current score: {score}")
+    else:
+        print(f"Sorry, that's wrong. Final score: {score}")
+        game_should_continue = False
 
 
 # Score keeping
